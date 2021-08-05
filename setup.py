e3a02f4e914fa7902303b6d8ee295be435764ac9
Zelow <83988054+zeloww@users.noreply.github.com>
2021-08-05 17:04:48 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="letters",
+    version='1.0',
+    license='MIT',
+    authors=["zelow"],
+    description="Change the font of your texts in 2 clicks!",
+    long_description='Documentation: https://github.com/zeloww/letters',
+    keywords=['python', 'py', 'letter', 'letters', 'font', 'fonts', 'text'],
+    packages=['letters']
+)
