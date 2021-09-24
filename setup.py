f14cdc270baf5172f5cf799cb8f3dcaebe335fd6
Zelow <83988054+zeloww@users.noreply.github.com>
2021-09-24 23:15:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
+import pypandoc
+
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    long_description = long_description.replace('\r', '')
+
+except OSError:
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
 
 setup(
-    name="letters",
-    version='1.0',
-    license='MIT',
-    authors=["zelow"],
-    description="Change the font of your texts in 2 clicks!",
-    long_description='Documentation: https://github.com/zeloww/letters',
-    keywords=['python', 'py', 'letter', 'letters', 'font', 'fonts', 'text'],
+    name='letters',
+    version='1.5',
+    license='GPLv3+',
+    authors=['zelow'],
+    url='https://github.com/zeloww/letters',
+    description='A simple method for customize your texts!',
+    long_description=long_description,
+    keywords=['python', 'py', 'letter', 'letters', 'font', 'ascii', 'color', 'colors', 'gradient', 'gradients', 'fade' 'text'],
     packages=['letters']
 )
